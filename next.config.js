c52967044b3847d140e8b807dc84ab9b59d465e0
Ricardo Rodrigues <ricardor662@gmail.com>
2024-05-02 20:20:11 -0300

    update packages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-module.exports = {
-  // Webpack 5 is enabled by default
-  // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
-  webpack5: false,
-};
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+}
+ 
+module.exports = nextConfig
